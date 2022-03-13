fa9460ced110d4be531a7d603845185b167796f5
a24230928 <a24230928@gmail.com>
2022-03-13 12:56:22 +0800

    fix: use bash to fix source not found problem

--- installl.sh
+++ installl.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # install powerlevel10k
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 sed -i 's|ZSH_THEME=".*"|ZSH_THEME="powerlevel10k/powerlevel10k"|' ~/.zshrc
